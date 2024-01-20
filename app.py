c093e2acbaef84d132c3609d28aa431039b780ee
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2024-01-20 13:35:54 +0000

    Give it some additional info

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import tkinter as tk
-from yt_dlp import YoutubeDL
+import threading
+
+mainscreen = tk.Tk("DreckPort-Downloader")
+label = tk.Label(mainscreen, text="URL")
+text = tk.Entry(mainscreen)
+
 
 def ClickButton():
     print("Button clicked")
 
-mainscreen = tk.Tk("DreckPort-Downloader")
-
-text = tk.Label(mainscreen, text="Test")
 button = tk.Button(mainscreen, text="Click me", command=ClickButton)
 text.pack()
 button.pack()
+
 mainscreen.mainloop()
+
+import json
+import yt_dlp
+
+URL = 'https://www.youtube.com/watch?v=BaW_jenozKc'
+
+
+ydl_opts = {}
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info(URL, download=False)
+
+    # ℹ️ ydl.sanitize_info makes the info json-serializable
+    print(json.dumps(ydl.sanitize_info(info)))
